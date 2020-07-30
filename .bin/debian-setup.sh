85246ecd9c272af08f465fb33f4ab73fe2333496
Tushar Khan <tushar.54k@gmail.com>
2020-07-29 21:14:03 -0400

    remove firefox

--- .bin/debian-setup.sh
+++ .bin/debian-setup.sh
@@ -1,41 +1,51 @@
 #!/bin/bash
 # This script installs commands and applications for my Debian setup
 
+# Verify that script is being executed by root
 if [ $USER != 'root' ]; then
     echo -e You must be "\e[1m\e[31mroot\e[0m" to run this script
     exit
 fi
 
-apt-get update -y
+# Uninstall Mozilla Firefox
+apt-get purge firefox firefox-esr
 
-echo 'Installing packages'
+if [ -d /home/$USER/.mozilla ]; then
+    rm -rf /home/$USER/.mozilla
+fi
+if [ -d /home/$USER/.cache/mozilla ]; then
+    rm -rf /home/$USER/.cache/mozilla
+fi
+
+# Install packages
+apt-get update -y
 apt-get install `tr '\r\n' ' ' < packages` -y
 
-# install required commands to install applications
+# Install packages required to install applications
 apt-get install apt-transport-https ca-certificates curl software-properties-common gnupg-agent -y
 
+# Install Google Chrome
 # https://linuxize.com/post/how-to-install-google-chrome-web-browser-on-debian-10/
-echo 'Installing Google Chrome...'
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb -y
-rm google-chrome-stable_current_amd64.deb
+apt-get install ./google-chrome-stable_current_amd64.deb -y
+rm -f google-chrome-stable_current_amd64.deb
 
+# Install Visual Studio Code
 # https://linuxize.com/post/how-to-install-visual-studio-code-on-ubuntu-18-04/
-echo 'Installing Visual Studio Code...'
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
 add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 apt-get update -y
 apt-get install code -y
 
+# Install Spotify
 # https://www.spotify.com/us/download/linux/
-echo 'Installing Spotify...'
 curl -sS https://download.spotify.com/debian/pubkey.gpg | apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
 apt-get update -y
 apt-get install spotify-client -y
 
+# Install Docker
 # https://docs.docker.com/engine/install/debian/
-echo 'Installing Docker...'
 apt-get remove docker docker-engine docker.io containerd runc
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
