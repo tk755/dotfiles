597f6919b9b7c789cbbf9940db18793b893c68da
Tushar Khan <tushar.54k@gmail.com>
2020-08-01 17:16:55 -0400

    added packages

--- .bin/debian-setup.sh
+++ .bin/debian-setup.sh
@@ -18,11 +18,10 @@ if [ -d /home/$USER/.cache/mozilla ]; then
 fi
 
 # Install packages
-apt-get update -y
-apt-get install `tr '\r\n' ' ' < packages` -y
+apt-get update -y && apt-get install `tr '\r\n' ' ' < packages` -y
 
 # Install packages required to install applications
-apt-get install apt-transport-https ca-certificates curl software-properties-common gnupg-agent -y
+apt-get install apt-transport-https ca-certificates curl software-properties-common gnupg-agent python3-pip -y
 
 # Install Google Chrome
 # https://linuxize.com/post/how-to-install-google-chrome-web-browser-on-debian-10/
@@ -34,21 +33,31 @@ rm -f google-chrome-stable_current_amd64.deb
 # https://linuxize.com/post/how-to-install-visual-studio-code-on-ubuntu-18-04/
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
 add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-apt-get update -y
-apt-get install code -y
+apt-get update -y && apt-get install code -y
 
 # Install Spotify
 # https://www.spotify.com/us/download/linux/
 curl -sS https://download.spotify.com/debian/pubkey.gpg | apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
-apt-get update -y
-apt-get install spotify-client -y
+apt-get update -y && apt-get install spotify-client -y
 
 # Install Docker
 # https://docs.docker.com/engine/install/debian/
 apt-get remove docker docker-engine docker.io containerd runc
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-apt-get update -y
-apt-get install docker-ce docker-ce-cli containerd.io -y
+apt-get update -y && apt-get install docker-ce docker-ce-cli containerd.io -y
 usermod -aG docker $USER
+
+# Install VirtualBox
+# https://linuxize.com/post/how-to-install-virtualbox-on-debian-10/
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
+add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
+apt-get update -y && apt-get install virtualbox-6.0 -y
+
+# Powerline
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mv PowerlineSymbols.otf /usr/share/fonts/X11/misc/
+fc-cache -vf /usr/share/fonts/X11/misc/
