fca598eb7fadf5c133773313528503cf5f19b240
Tushar Khan <tushar.54k@gmail.com>
2020-10-20 18:11:40 -0400

    i3 config and scripts updates

---
+++ .config/i3/restart.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+BIN="$HOME/code/scripts/"
+
+# monitor configuration
+source "${BIN}display-manager"
+
+# set background image
+source "${BIN}background-manager"
---
+++ .config/i3/startup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# launch bluetooth adapter
+source /usr/bin/blueman-applet
+
+# launch dropbox
+dropbox start
