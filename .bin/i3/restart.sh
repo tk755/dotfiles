0504b031cdfa9348bb136b692d31777342f657d2
Tushar Khan <tushar.54k@gmail.com>
2021-02-13 04:03:07 -0500

    minor changes

--- .bin/i3/restart.sh
+++ .bin/i3/restart.sh
@@ -3,11 +3,12 @@
 # disable laptop keyboard
 python3 $HOME/bin/keyboard/auto.py
 
+# set background image
+PIC_DIR=$(xdg-user-dir PICTURES)
+feh --bg-fill $PIC_DIR/desktops/spaceman.jpeg
+
 # monitor configuration
 source $HOME/bin/kawasaki/display.sh
-
-# set background image
-source $HOME/bin/kawasaki/bg.sh
 
 # launch polybar
 source $HOME/.config/polybar/launch.sh
--- .bin/kawasaki/reset-network.sh
+++ .bin/kawasaki/reset-network.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This program reconnects the the network module to the kernel.
+# Useful for reconnecting to wifi.
+
 modprobe -r brcmfmac
 modprobe brcmfmac
 
