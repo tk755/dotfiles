561b58de8927246d9c3cd28e3dba3c03587b1d0c
Tushar Khan <tushar.54k@gmail.com>
2021-02-13 23:49:53 -0500

    polybar changes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-## Add this to your wm startup file.
+# Add to i3 startup
 
 # Terminate already running bar instances
 killall -q polybar
@@ -8,5 +8,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
+# Launch bars
+# polybar -c ~/.config/polybar/config.ini main &
+# polybar -c ~/.config/polybar/config.ini main
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
+  done
+else
+  polybar -c ~/.config/polybar/config.ini main &
+fi
