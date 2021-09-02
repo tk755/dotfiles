82b85aa97ce270966ef29d5c3df25fdb85ba6ab2
Tushar Khan <tushar.54k@gmail.com>
2021-09-02 19:27:22 -0400

    updates

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -3,9 +3,14 @@
 
 # commands
 alias title='clear; source ~/.bashrc'
+alias pdfconvert='libreoffice --headless --convert-to pdf' # file
+alias pdfcompile='pandoc --pdf-engine=pdflatex -f markdown -o' # out.pdf src.md
 
 # dotfiles bare repository
 alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+
+# common mistypes
+alias sl='ls'
 
 # better utilities
 alias mv="mv -iv"
@@ -15,7 +20,6 @@ alias mkdir="mkdir -vp"
 alias rmdir="rmdir -v"
 
 # color
-alias sl='ls'
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias tree='tree --dirsfirst -C'
