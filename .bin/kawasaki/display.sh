417434a80ec614dc0fbed0468a7f2e16eb4fb237
Tushar Khan <tushar.54k@gmail.com>
2021-09-04 17:16:05 -0400

    minor change

--- .bin/kawasaki/display.sh
+++ .bin/kawasaki/display.sh
@@ -4,4 +4,4 @@
 # xrandr --output eDP-1 --mode 1920x1200
 xrandr --output eDP-1 --mode 1680x1050
 
-xrandr --output HDMI-2 --mode 2560x1440 --left-of eDP-1
+xrandr --output HDMI-2 --mode 2560x1440 --right-of eDP-1
