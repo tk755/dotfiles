5a412f55ff23260f7937e38fca8a1431ebe248c0
Tushar Khan <tushar.54k@gmail.com>
2021-09-05 15:49:20 -0400

    fixed i3 gaps dependenciies

--- .bin/distro-setup/packages/apt-install.sh
+++ .bin/distro-setup/packages/apt-install.sh
@@ -35,7 +35,7 @@ fi
 # https://github.com/Airblader/i3/wiki/Building-from-source
 if ask_install "i3 gaps" ; then
     # install dependencies
-    apt-get install dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev -y
+    apt-get install meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev -y
 
     # clone the repository
     CWD=$(pwd)
