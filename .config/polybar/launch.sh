8a54ffa8aa582bc694adf18609cfbd2ef090c9cf
Tushar Khan <tushar.54k@gmail.com>
2021-09-07 19:22:20 -0400

    terminate bars using pkill instead of killall

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,7 +3,7 @@
 # Add to i3 startup
 
 # Terminate already running bar instances
-killall -q polybar
+pkill polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
