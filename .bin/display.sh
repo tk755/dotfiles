1e11b6010e4fea0436310e6075a08b2b29cc1efc
Tushar Khan <tushar.54k@gmail.com>
2021-09-07 18:54:07 -0400

    refactored bin

--- .bin/display.sh
+++ .bin/display.sh
@@ -2,6 +2,10 @@
 
 # xrandr --output eDP-1 --mode 2560x1600
 # xrandr --output eDP-1 --mode 1920x1200
-xrandr --output eDP-1 --mode 1680x1050
+
+case $HOSTNAME in
+  kawasaki) xrandr --output eDP-1 --mode 1680x1050 ;;
+  yamaha) xrandr --output eDP-1 --mode 1920x1080 ;;
+esac
 
 xrandr --output HDMI-2 --mode 2560x1440 --right-of eDP-1
--- .bin/i3/restart.sh
+++ .bin/i3/restart.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # disable laptop keyboard
-python3 $HOME/bin/keyboard/auto.py
+$HOME/bin/keyboard/auto.py
+
+# monitor configuration
+$HOME/bin/display.sh
 
 # set background image
 PIC_DIR=$(xdg-user-dir PICTURES)
 feh --bg-fill $PIC_DIR/desktops/spaceman.jpeg
 
-# monitor configuration
-source $HOME/bin/kawasaki/display.sh
-
 # launch polybar
-source $HOME/.config/polybar/launch.sh
+$HOME/.config/polybar/launch.sh
