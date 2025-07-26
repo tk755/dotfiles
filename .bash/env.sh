0e5ab72cdc00cc52aa7592b674a8377103221b60
Tushar Khan <tushar.54k@gmail.com>
2025-07-26 00:40:01 -0400

    update env paths

--- .bash/env.sh
+++ .bash/env.sh
@@ -1,20 +1,24 @@
-export PATH="$PATH"
-
-# add user's private bin directories
-if [ -d "$HOME/bin" ]; then
-    PATH="$HOME/bin:$PATH"
+# add machine-specific user scripts to path
+if [ -d "$HOME/.bin/$(hostname)" ]; then
+    PATH="$HOME/.bin/$(hostname):$PATH"
 fi
 
+# add shared user scripts to path
+if [ -d "$HOME/.bin" ]; then
+    PATH="$HOME/.bin:$PATH"
+fi
+
+# add standard user scripts to path
 if [ -d "$HOME/.local/bin" ]; then
     PATH="$HOME/.local/bin:$PATH"
 fi
 
-# add OCaml merlin path
+# add OCaml merlin to path
 if [ -d "$HOME/.opam/default/bin" ]; then
     PATH="$HOME/.opam/default/bin:$PATH"
 fi
 
-# add Go binary path
+# add Go binary to path
 if [ -d "/usr/local/go/bin" ]; then
     PATH="/usr/local/go/bin:$PATH"
 fi
